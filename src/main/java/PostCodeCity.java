37217ca5cb45a06c722554074860fe2291549797
TSieduszewski <tsieduszewski@gmail.com>
2020-12-16 00:54:21 +0100

    Zmodyfikowana wersja generatora - poprzednia wersja miałą mankament w postaci sztywnego adresu bazy adresów, co spowodowałoby problemy a) przy wrzuceniu apki do testów, wykrzaczyłoby generator bo nie mógłby znaleźć bazy b) docelowo cała apka ma być JARem, więc sztywny adres bazy również by nie zadziałał. Rozwiązanie to zastosowanie InputStreamów.

--- src/main/java/PostCodeCity.java
+++ src/main/java/PostCodeCity.java
@@ -1,13 +1,19 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class PostCodeCity {
 
     private final ArrayList<String> listOfPostCodeCity;
+    private PostCodeCityList postCodeCityList;
     private final Random random;
     private JSONArray jsonArray;
 
@@ -17,21 +23,39 @@ public class PostCodeCity {
     }
 
     public void drawData(int numberOfDrawedValues) throws Exception {
+
+        //Barney style
         jsonMethod();
         for (int i = 0; i <= numberOfDrawedValues; i++) {
-            int num = random.nextInt(jsonArray.size());
-            listOfPostCodeCity.add(jsonArray.get(num).toString());
+            int num = random.nextInt(getPostCodeCityList().getPostCodeCityList().size());
+            listOfPostCodeCity.add(getPostCodeCityList().getPostCodeCityList().get(num));
         }
-    }
-
-    public ArrayList<String> getDrawedListOfPostCodeCity(){
-        return listOfPostCodeCity;
+        //casual style
+//        jsonMethod();
+//        for (int i = 0; i <= numberOfDrawedValues; i++) {
+//            int num = random.nextInt(jsonArray.size());
+//            listOfPostCodeCity.add(jsonArray.get(num).toString());
+//        }
     }
 
     private void jsonMethod() throws Exception {
-        JSONParser jsonParser = new JSONParser();
-        //potrzebuję lepszego rozwiązania odczytu pliku .json
-        JSONObject jsonObject = (JSONObject) jsonParser.parse((new FileReader("src/main/resources/postCodeCityList.json")));
-        jsonArray = (JSONArray) jsonObject.get("postCodeCityList");
+        InputStream inputStream = getClass().getResourceAsStream("/postCodeCityList.json");
+
+        //Barney style
+        ObjectMapper objectMapper = new ObjectMapper();
+        postCodeCityList = objectMapper.readValue(inputStream, PostCodeCityList.class);
+
+//        //casual style
+//        JSONParser jsonParser = new JSONParser();
+//        JSONObject jsonObject = (JSONObject) jsonParser.parse(new InputStreamReader(inputStream));
+//        jsonArray = (JSONArray) jsonObject.get("postCodeCityList");
+    }
+
+    public final PostCodeCityList getPostCodeCityList(){
+        return postCodeCityList;
+    }
+
+    public final ArrayList<String> getDrawedListOfPostCodeCity(){
+        return listOfPostCodeCity;
     }
 }
