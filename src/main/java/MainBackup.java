d81c769e7a2fc8ea2caaec37a91b6ae0a079ad82
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-23 07:22:28 +0100

    #4 przeniesienie zmian z feature_branch#4

--- src/main/java/MainBackup.java
+++ src/main/java/MainBackup.java
@@ -1,26 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class MainBackup {
     private static final Scanner scanner = new Scanner(System.in);
-    private static final StreetsNr streetsList = new StreetsNr();
+    private static final StreetsNr lists = new StreetsNr();
 
+    public static void main(String[] args) throws IOException {
+        generateStreetsNames();
 
-    public static void main(String[] args) {
         boolean quit = true;
         int choice = 0;
         printInstructions();
 
-        while(quit){
+        while (quit) {
             System.out.println("Enter your choice:");
             choice = scanner.nextInt();
             scanner.nextLine();
 
-            switch(choice){
+            switch (choice) {
                 case 0:
                     printInstructions();
                     break;
                 case 1:
-                    streetsList.printStreetsList();
+                    lists.printStreetsList();
                     break;
                 case 2:
                     addName();
@@ -29,6 +34,9 @@ public class MainBackup {
                     findStreet();
                     break;
                 case 4:
+//                    drawStreet();
+                    break;
+                case 5:
                     quit = false;
                     break;
             }
@@ -36,26 +44,50 @@ public class MainBackup {
 
     }
 
-    public static void printInstructions(){
+    // Reads list of street from streets.txt and adds them to an ArrayList. -> streetsList
+    public static void generateStreetsNames() throws IOException {
+
+        InputStream is = StreetsNr.class.getResourceAsStream("streets.txt");
+        try {
+
+            BufferedReader file = new BufferedReader(new InputStreamReader(is));
+            for (; ; ) {
+                String line = file.readLine();
+                if (line == null) {
+                    break;
+                }
+                lists.streetsList.add(line);
+            }
+
+            System.out.println("End.");
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public static void printInstructions() {
         System.out.println("\nPress ");
         System.out.println("\t 0 - To print choice options.");
         System.out.println("\t 1 - To print the list of streets.");
         System.out.println("\t 2 - To add a street to the list");
         System.out.println("\t 3 - To search for a street in the list");
-        System.out.println("\t 4 - To quit the application");
+        System.out.println("\t 4 - To generate a number of streets from the list");
+        System.out.println("\t 5 - To quit the application");
     }
 
-    public static void addName(){
+    public static void addName() {
         System.out.print("Please enter the street name: ");
-        streetsList.addStreetName(scanner.nextLine());
+        lists.addStreetName(scanner.nextLine());
     }
 
-    public static void findStreet(){
+    public static void findStreet() {
         System.out.print("Street name to search for: ");
         String searchItem = scanner.nextLine();
-        if(streetsList.findAStreet(searchItem) !=null){
+        if (lists.findAStreet(searchItem) != null) {
             System.out.println("Found " + searchItem + " street in our list.");
-        }else{
+        } else {
             System.out.println(searchItem + " is not in the list");
         }
     }
--- src/main/java/StreetsNr.java
+++ src/main/java/StreetsNr.java
@@ -1,26 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-public class StreetsNr{
-
-    private final ArrayList<String> streetsList = new ArrayList<String>();
+import java.util.Collections;
+import java.util.Random;
 
 
-    public void addStreetName (String strName){
-    streetsList.add(strName);
+public class StreetsNr {
 
-}
+    public ArrayList<String> streetsList = new ArrayList<String>();
+
+    Random rand = new Random();
+
+    //Draws random number of streets from the ArrayList.
+    public void drawStreets() {
+        Random r = new Random();
+        int pos = streetsList.indexOf(r);
+        System.out.println(pos);
+    }
+
+    public void addStreetName(String strName) {
+
+        streetsList.add(strName);
+
+    }
 
     public void printStreetsList() {
         System.out.println("You have " + streetsList.size() + " streets on your list");
-        for(int i = 0; i < streetsList.size(); i++){
-            System.out.println((i+1) + ". " + streetsList.get(i));
+        for (int i = 0; i < streetsList.size(); i++) {
+            System.out.println((i + 1) + ". " + streetsList.get(i));
         }
     }
 
-    public String findAStreet(String searchItem){
+    public String findAStreet(String searchItem) {
 
         int position = streetsList.indexOf(searchItem);
-        if(position >=0){ // Because if the .idexOf function will return -1, it will mean that the file don't exists.
+        if (position >= 0) { // Because if the .idexOf function will return -1, it will mean that the file don't exists.
             return streetsList.get(position);
         }
         return null;
