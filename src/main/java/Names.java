128e25d2bf6c0b03352bf22aa61e79d9a8756667
TSieduszewski <tsieduszewski@gmail.com>
2021-01-14 23:57:56 +0100

    Rozpoczęcie zadania #1 - na razie sama klasa z rozpiską co jest do zrobienia

---
+++ src/main/java/Names.java
@@ -0,0 +1,11 @@
+public class Names {
+
+    //dodaj bazę imion jako zewnętrzny plik (JSON?) (znajdź bazę imion w internecie)
+
+    //w bazie uwzględnij płeć imienia. Może jako lista obiektów?
+
+    //zaciągnij imiona do Javy w taki sposób, aby przy imieniu dało się wygenerować również płeć
+    // (1-mężczyzna 2-kobieta - to będą inty)
+
+    //stwórz statyczną metodę zwracającą listę imion (jako obiekty?)
+}
