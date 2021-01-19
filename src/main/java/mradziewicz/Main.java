b364661855547204f9e853dd78b2664a4b080a57
Michał <Radziewicz94@gmail.com>
2021-01-19 21:12:10 +0100

    konwersja tablicy z objektu na Stringa

--- src/main/java/mradziewicz/Main.java
+++
@@ -1,9 +0,0 @@
-package mradziewicz;
-
-public class Main {
-    public static void main(String[] args) {
-        DateAndPeselGenerator dateAndPeselGenerator = new DateAndPeselGenerator();
-        dateAndPeselGenerator.createRandomDate(100);
-        dateAndPeselGenerator.getDate();
-    }
-}
