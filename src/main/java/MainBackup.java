ca05fc92274f0c6dce6596f74d8c643afacefc92
Alexei A <alexey.alexandrenko@gmail.com>
2021-01-19 19:43:50 +0100

    Jest gotowy do review. Brakuje tylko wylosowania numerow ulic/domow - do ogarneniaj pozniej

--- src/main/java/MainBackup.java
+++ src/main/java/MainBackup.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class MainBackup {
 
     public static void main(String[] args) throws IOException {
+
         StreetsNr test = new StreetsNr();
         System.out.println(test.generateStreetsList(5));
 
--- src/main/java/StreetsNr.java
+++ src/main/java/StreetsNr.java
@@ -4,21 +4,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
 public class StreetsNr {
 
-    private final ArrayList<String> streetsList = new ArrayList<>();
-    Random r = new Random();
-    Scanner s = new Scanner(System.in);
+    private final ArrayList<String> streetsList = new ArrayList<>(); // Lista do ktorej wczytuje nazwe ulic z JSON'a
+    private final ArrayList<String> newStreetsList = new ArrayList<>(); // Lista do ktorej się zapisują wylosowane ulicy
+    private final Random rand = new Random();
 
+    public ArrayList<String> generateStreetsList(int numberToDraw) throws IOException {  // Reads list of streets from streets.txt/json and adds them to an ArrayList. -> streetsList
 
-    public ArrayList<String> generateStreetsList(int numberToDraw) throws IOException {
-        // Reads list of streets from streets.txt/json and adds them to an ArrayList. -> streetsList
         try (InputStream is = StreetsNr.class.getResourceAsStream("/streets.json")) {
 
             BufferedReader file = new BufferedReader(new InputStreamReader(is));
-            for (; ; ) {
+            for ( ; ; ) {
                 String line = file.readLine();
                 if (line == null) {
                     break;
@@ -28,34 +26,27 @@ public class StreetsNr {
 
         }
 
-        //The loop will reapeat number of times, each time printing 1 name and 1 number, until count i meet entered numberToDraw.
-        for (int i = 0; i <= numberToDraw ; i++ ) {
+        //The loop will reapeat number of times, each time adding 1 name and 1 number, until count "i" will meet entered numberToDraw.
+        for (int i = 1; i <= numberToDraw; i++) {
 
-            //Draws random number of streets from the ArrayList.
+            //Draws random number of streets from the ArrayList and adding it to a new ArrayList.
+            int rand = numberToDraw;
+            newStreetsList.add(streetsList.get(rand));
 
-            numberToDraw = s.nextInt();
-            for (int count = 1; count <= numberToDraw; count++) {
-                int rand = r.nextInt(4901);
-                System.out.println(streetsList.get(rand).indexOf(rand));
-            }
+            // wylosowanie nr budynku (NIE WIEM JAK ZROBIC LOSOWONIE NUMEROW OSOBNO Z JSON'a)
 
-            // wylosowanie nr budynku
+//            numberToDraw = s.nextInt();
+//            for (int count = 1; count <= numberToDraw; count++) {
+//                int rand = r.nextInt(4901);
+//                System.out.println(streetsList.get(rand).indexOf(rand));
 
-            numberToDraw = s.nextInt();
-            for (int count = 1; count <= numberToDraw; count++) {
-                int rand = r.nextInt(4901);
-                System.out.println(streetsList.get(rand).indexOf(rand));
-            }
-
-            // zlaczenie ulicy z wylosowanym numerem
-
-
-            // przerobienie na stringa
-
+            // zlaczenie ulicy z wylosowanym numereem & przerobienie na stringa
             // dodanie do arraylisty (finalnej listy)
 
-
         }
-        return streetsList;
+
+        return newStreetsList;
     }
+
 }
+
